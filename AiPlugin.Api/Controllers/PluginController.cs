1fa2aebf374eb71d3a9aa90463e3f4741373ea29
Michele Bortot <31648634+EkimoCode@users.noreply.github.com>
2023-04-18 22:00:17 +0200

    mocked apis

--- AiPlugin.Api/Controllers/PluginController.cs
+++ AiPlugin.Api/Controllers/PluginController.cs
@@ -7,9 +7,31 @@ namespace AiPlugin.Api.Controllers
     public class PluginController : ControllerBase
     {
         [HttpGet("{userId}/.well-known/ai-plugin.json")]
-        public string Get(Guid userId)
+        public async Task<string> Get(Guid userId)
         {
-            return "{hello: world}";
+            await Task.Delay(4000);
+            //gets the plugin doc from the db
+            return """{"hello": "world"}""";
+        }
+
+        [HttpGet("{userId}/{actionId}")]
+        public async Task<string> Get(Guid userId, Guid actionId)
+        {
+            await Task.Delay(4000);
+            //gets the plugin action from the db
+            return """{"hello": "world"}""";
+        }
+
+        [HttpPost("{userId}")]
+        public async Task<string> Get(Guid userId, [FromBody] string content)
+        {
+            await Task.Delay(4000);
+            return $$$"""
+            {
+                "userId":"{{{userId.ToString()}}}",
+                "aiPlugin": "{{{content}}}"
+            }
+            """;
         }
     }
 }
--- AiPlugin.Api/Controllers/UserController.cs
+++ AiPlugin.Api/Controllers/UserController.cs
@@ -1,16 +1,14 @@
-using Microsoft.AspNetCore.Mvc;
+// using Microsoft.AspNetCore.Mvc;
 
-namespace AiPlugin.Api.Controllers
-{
-    [ApiController]
-    [Route("[controller]")]
-    public class UserController : ControllerBase
-    {
-        [HttpPut]
-        public Guid Create()
-        {
-            return new Guid();
-            //todo save in db
-        }
-    }
-}
+// namespace AiPlugin.Api.Controllers
+// {
+//     [ApiController]
+//     [Route("[controller]")]
+//     public class UserController : ControllerBase
+//     {
+//         [HttpPost]
+//         public async Task<string> Get(Guid userId, [FromBody] string content)
+//         {
+//             return new Guid().ToString();
+//         }
+//     }
