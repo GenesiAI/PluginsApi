605bf76cdb0ac05d72fa764757590a90dd149fe3
Michele Bortot <31648634+EkimoCode@users.noreply.github.com>
2023-08-20 15:34:26 +0200

    refactored program for clarity

--- Api/Program.cs
+++ Api/Program.cs
@@ -11,62 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Logging.AddConsole();
 
-//var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-//builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
-
-// Add services to the container.
-builder.Services.AddHttpClient();
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: "MyDevelopmentPolicy",
-        builder =>
-        {
-            builder.WithOrigins("http://localhost:3000")
-                    .AllowAnyHeader()
-                    .AllowAnyMethod();
-        });
-});
-
 var version = "1.2.2"; //pluginId and admin subdomain management with authenticated users
-builder.Services.AddSwaggerGen(options =>
-{
-    options.OperationFilter<OpenApiParameterIgnoreFilter>();
-    options.SwaggerDoc(version, new OpenApiInfo
-    {
-        Title = "Genesi AI Plugin API",
-        Version = version,
-        Description = "API set to get and manage Plugins. routes are intended to be accessed on subdomains in the format {pluginId}.Genesi.AI."
-    });
-});
-
-
-builder.Services.AddScoped<IBaseRepository<Plugin>, PluginRepository>();
-builder.Services.AddScoped<SubscriptionRepository>();
-
-builder.Services.AddAuthentication(options => { options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; })
-      .AddJwtBearer(options =>
-      {
-          //options.AutomaticAuthenticate = true;
-          options.IncludeErrorDetails = true;
-          options.Authority = "https://securetoken.google.com/genesi-ai";
-          options.TokenValidationParameters = new TokenValidationParameters
-          {
-              ValidateIssuer = true,
-              ValidIssuer = "https://securetoken.google.com/genesi-ai",
-              ValidateAudience = true,
-              ValidAudience = "genesi-ai",
-              ValidateLifetime = true
-          };
-      });
-
-// add database
-builder.Services.AddDbContext<AiPluginDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+AddServices(builder, version);
 
 var app = builder.Build();
 
@@ -95,3 +41,71 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+static void AddServices(WebApplicationBuilder builder, string version)
+{
+
+    //var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+    //builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+
+    // Add services to the container.
+    builder.Services.AddHttpClient();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy(name: "MyDevelopmentPolicy",
+            builder =>
+            {
+                builder.WithOrigins("http://localhost:3000")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+            });
+    });
+
+    builder.Services.AddSwaggerGen(options =>
+    {
+        options.OperationFilter<OpenApiParameterIgnoreFilter>();
+        options.SwaggerDoc(version, new OpenApiInfo
+        {
+            Title = "Genesi AI Plugin API",
+            Version = version,
+            Description = "API set to get and manage Plugins. routes are intended to be accessed on subdomains in the format {pluginId}.Genesi.AI."
+        });
+    });
+
+
+    builder.Services.AddScoped<IBaseRepository<Plugin>, PluginRepository>();
+    builder.Services.AddScoped<SubscriptionRepository>();
+
+    builder.Services.AddAuthentication(options => { options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; })
+          .AddJwtBearer(options =>
+          {
+              //options.AutomaticAuthenticate = true;
+              options.IncludeErrorDetails = true;
+              options.Authority = "https://securetoken.google.com/genesi-ai";
+              options.TokenValidationParameters = new TokenValidationParameters
+              {
+                  ValidateIssuer = true,
+                  ValidIssuer = "https://securetoken.google.com/genesi-ai",
+                  ValidateAudience = true,
+                  ValidAudience = "genesi-ai",
+                  ValidateLifetime = true
+              };
+          });
+
+    // add database
+    builder.Services.AddDbContext<AiPluginDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+    builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+}
+
+
+// public class StripeSettings
+// {
+//     public string WebHookSecret = null!;
+//     public string PremiumPriceId = null!;
+//     public string ApiKey = null!;
+// }
