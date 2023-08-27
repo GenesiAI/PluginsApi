5d93629d55921212d0010e7cc45f5caac547a189
Michele Bortot <31648634+EkimoCode@users.noreply.github.com>
2023-08-27 18:45:47 +0200

    cleaned up get methods

--- Application/common/IBaseRepository.cs
+++ Application/common/IBaseRepository.cs
@@ -7,7 +7,6 @@ public interface IBaseRepository<T> where T : EntityBase
 {
     public Task<T> Add(T entity, string userId, CancellationToken cancellationToken = default);
     public Task<T> Get(Guid id, CancellationToken cancellationToken = default);
-    public IQueryable<Plugin> Get(CancellationToken cancellationToken = default);
     public Task<T> Update(T entity, CancellationToken cancellationToken = default);
     public Task Delete(Guid id, CancellationToken cancellationToken = default);
 }
