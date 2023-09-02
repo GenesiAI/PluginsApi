05d0fd3da7825a349d09071d8535566a1ad0b509
Michele Bortot <31648634+EkimoCode@users.noreply.github.com>
2023-09-02 20:03:49 +0200

    test of redeploy

--- Domain/Subscription/StripeSettings.cs
+++
@@ -1,6 +0,0 @@
-public class StripeSettings
-{
-    public string WebHookSecret { get; set; } = null!;
-    public string PremiumPriceId { get; set; } = null!;
-    public string ApiKey { get; set; } = null!;
-}
